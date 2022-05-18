71ee0d6eceff76f3b2f0bb452eebbd714e1ea277
Hess Wang <hess90wang@gmail.com>
2022-05-18 09:23:04 +0900

    0517

---
+++ react-fullstack/09-axios-hooks/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Titanic from './pages/Titanic';
+
+function App() {
+  return (
+    <>
+      <h1>10-Axios-Hooks</h1>
+      <Titanic />
+    </>
+  );
+}
+
+export default App;
